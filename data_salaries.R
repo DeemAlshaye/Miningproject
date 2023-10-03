db56075762224ef39808ab6d504c0a31cd53659c
DeemAlshaye <144781084+DeemAlshaye@users.noreply.github.com>
2023-10-03 17:12:00 +0300

    Add R code for data_salaries

---
+++ data_salaries.R
@@ -0,0 +1,35 @@
+#missing values
+sum(is.na(ds_salaries))
+
+#statical mesurment
+summary(ds_salaries$salary)
+
+summary(ds_salaries$salary_in_usd)
+summary(ds_salaries$remote_ratio)
+
+#finding outliars
+boxplot.stats(ds_salaries$salary)$out
+boxplot.stats(ds_salaries$salary_in_usd)$out              
+boxplot.stats(ds_salaries$remote_ratio)$out
+
+
+#data cleaning
+
+#checking for missing values
+dim(ds_salaries)
+
+ds_salaries=na.omit(ds_salaries)
+dim(ds_salaries)
+
+sum(is.na(ds_salaries))
+
+#removing the outliers
+
+outliers <- boxplot(ds_salaries$salary, plot=FALSE)$out
+ds_salaries <- ds_salaries[-which(ds_salaries$salary%in% outliers),]
+boxplot.stats(ds_salaries$salary)$out
+
+outliers <- boxplot(ds_salaries$salary_in_usd, plot=FALSE)$out
+ds_salaries <- ds_salaries[-which(ds_salaries$salary_in_usd%in% outliers),]
+boxplot.stats(ds_salaries$salary_in_usd)$out
+
